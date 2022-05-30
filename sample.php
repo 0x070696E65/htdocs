d4317cd7f54ddee561ea7bae5b45cc0fbf284a85
toshiya matsukawa <matsukawa5955@gmail.com>
2022-05-30 22:38:15 +0300

    キーの修正

--- sample.php
+++ sample.php
@@ -4,12 +4,12 @@ use crypto\snipets;
 
 $snipets = new snipets;
 
-$pubKey = "43CC385CF37318D022336624C8A56CBEB60360712D70163B554BA23EABF2D10E";
+$pubKey = "PUBLIC_KEY";
 $address = $snipets->addressToString($snipets->publicKeyToAddress($pubKey, 152));
 echo($address);
 echo PHP_EOL;
 
-$privatekey = "5E73378E058339952B13D65297C294884C03C83DECDAB2B9B3E33AFC8F89AA22";
-$encrypted = "1D14FBC75D99B2F1F5CA5AFD04B420898375A781E8E2435C5AFE4EAFE3FED855";
+$privatekey = "PRIVATE_KEY";
+$encrypted = "SSSなどで発行された暗号化済ペイロード";
 $payload = $snipets->decryptMessage($privatekey, $pubKey, $encrypted);
 echo($payload);
