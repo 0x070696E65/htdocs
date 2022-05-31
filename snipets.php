e9f3b76e3d7607f0d7b7bd840a77fc1ef030327b
toshiya matsukawa <matsukawa5955@gmail.com>
2022-05-31 07:04:59 +0300

    changed namespace

--- snipets.php
+++ snipets.php
@@ -3,18 +3,15 @@ namespace crypto;
 require_once('crypto/catapultCrypto.php');
 require_once('format/base32.php');
 use crypto\catapultCrypto;
-use crypto\Base32;
-$b32 = new Base32;
-$catapultCrypto = new catapultCrypto;
+use format\Base32;
 
 class snipets {
     function addressToString($decoded) {
-        global $b32;
+        $b32 = new Base32;
         $zero = array(0);
         $arr = array_merge($decoded, $zero);
         $packed = pack("c*", ...$arr);
-        $encoded = bin2hex($packed);
-        $decoded = pack('H*', $encoded);
+        $decoded = pack('H*', bin2hex($packed));
         return substr((string)$b32->encode($decoded), 0, 39);
     }
 
@@ -30,7 +27,7 @@ class snipets {
     }
 
     function decryptMessage($privateKey, $publicKey, $encrypted){
-        global $catapultCrypto;
+        $catapultCrypto = new catapultCrypto;
         $cipher = "aes-256-gcm";
         $encKey = $catapultCrypto->deriveSharedKey(array_map('hexdec', str_split($privateKey, 2)), array_map('hexdec', str_split($publicKey, 2)));
         $payload = substr(hex2bin($encrypted), 16 + 12);
